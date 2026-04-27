b08d0834f20153376c7811ae574dc53f6b4e3419
zakaichi.main <zakaichi.main@gmail.com>
2026-04-27 01:52:42 +0000

    feat: implement full PWA with maskable icon and service worker

--- sw.js
+++ sw.js
@@ -1,27 +1,26 @@
 const CACHE_NAME = 'adventure-diary-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/styles.css',
-  '/manifest.json',
-  // Database
-  '/database/items.json',
-  '/database/monsters.json',
-  '/database/skills.json',
-  // Logos and Icons
-  '/img/logo.png',
-  '/img/icons/brand_ico.png',
-  '/img/icons/items_ico.png',
-  '/img/icons/monsters_ico.png',
-  '/img/icons/skills_ico.png',
-  '/img/icons/quest_ico.png',
-  '/img/icons/book_ico.png',
-  '/img/icons/github_ico.png',
-  // Pages
-  '/item/index.html',
-  '/kontribusi/index.html',
-  '/monster/index.html',
-  '/skill/index.html'
+  '/adventure-diary/',
+  '/adventure-diary/index.html',
+  '/adventure-diary/styles.css',
+  '/adventure-diary/script.js',
+  '/adventure-diary/manifest.json',
+  '/adventure-diary/img/logo.png',
+  '/adventure-diary/img/icons/brand_ico.png',
+  '/adventure-diary/img/icons/items_ico.png',
+  '/adventure-diary/img/icons/monsters_ico.png',
+  '/adventure-diary/img/icons/skills_ico.png',
+  '/adventure-diary/img/icons/quest_ico.png',
+  '/adventure-diary/item/',
+  '/adventure-diary/item/index.html',
+  '/adventure-diary/monster/',
+  '/adventure-diary/monster/index.html',
+  '/adventure-diary/skill/',
+  '/adventure-diary/skill/index.html',
+  '/adventure-diary/kontribusi/',
+  '/adventure-diary/kontribusi/index.html',
+  '/adventure-diary/kontribusi/admin.html',
 ];
 
 self.addEventListener('install', event => {
@@ -29,7 +28,10 @@ self.addEventListener('install', event => {
     caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(urlsToCache.map(url => new Request(url, {cache: 'reload'})));
+      })
+      .catch(error => {
+          console.error('Failed to open cache: ', error);
       })
   );
 });
@@ -41,18 +43,7 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        return fetch(event.request).then(response => {
-          // Optionally, cache new requests dynamically
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-          const responseToCache = response.clone();
-          caches.open(CACHE_NAME)
-            .then(cache => {
-              cache.put(event.request, responseToCache);
-            });
-          return response;
-        });
+        return fetch(event.request);
       })
   );
 });
